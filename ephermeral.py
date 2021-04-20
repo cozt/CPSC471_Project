c844dd07fefb85ca361184909a4c59f9d216da8a
cozt <42822504+cozt@users.noreply.github.com>
2021-04-19 19:16:19 -0700

    Create ephermeral.py

---
+++ ephermeral.py
@@ -0,0 +1,14 @@
+import socket
+
+
+def do_ephemeral():
+
+    # Create a socket
+    welcomeSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # Bind the socket to port 0
+    welcomeSocket.bind(('',0))
+
+    print("I chose ephemeral port: ", welcomeSocket.getsockname()[1])
+
+    return welcomeSocket
