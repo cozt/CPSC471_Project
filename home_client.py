a91aee1a1102e1b15234f9560fbb52e7ae59d8de
cozt <42822504+cozt@users.noreply.github.com>
2021-04-19 19:14:39 -0700

    Update home_client.py

--- home_client.py
+++ home_client.py
@@ -1,15 +1,43 @@
-import socket
+from socket import *
+import ephemeral
 import sys
-argumentnum = len(sys.argv)
+import sendData as sd
+import receiveAllData as rAD
+import subprocess 
 
-if argumentnum==3:
-    servername= sys.argv[1]
-    serverport= sys.argv[2]
-    if serverport.isdigit():
-        serverport = int(serverport)
+
+if __name__ == "__main__":
+
+    numOfArguments = len(sys.argv)
+    if numOfArguments == 3:
+        serverName = sys.argv[1]
+        serverPort = sys.argv[2]
+        if serverPort.isdigit():
+            print("Server port is in right format")
+            serverPort = int(serverPort)
+        else:
+            print("Server port needs to be a digit")
+
+    # Create a socket
+    clientSocket = socket(AF_INET, SOCK_STREAM)
+
+    # Connect to the server
+    print("trying to connect to server")
+    clientSocket.connect((serverName, serverPort))
+
+    userInput = input("ftp> ")
+
+    if userInput.startswith('ls'):
+        print("Now printing out the directory")
+        # Tell the server we want to perform ls
+        subprocess.run('ls',shell = True)
+    elif userInput.startswith('get'):
+        subprocess.run('get', shell = True)
+    elif userInput.startswith('put'):
+        subprocess.run('put', shell = True)
+    elif userInput.startswith('quit'):
+        quit()
+
     else:
-        print("Please enter a digit")
-
-# creating a socket 
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect((servername,serverport))
+        print("Incorrect invocation. Client should be invoked as: client.py <server machine> <server port>")
+        quit()
